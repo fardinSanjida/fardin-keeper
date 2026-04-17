6d1260996b2d0b5ff7906c8bd743f78a0957f3c0
fardinSanjida <fputul71@gmail.com>
2026-04-17 17:13:05 +0600

    tags added

--- src/pages/homepages/Homepage.jsx
+++ src/pages/homepages/Homepage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Banner from '../../componenets/banner/Banner';
-import Footer from '../../componenets/footer/Footer';
 import AllFriends from '../../componenets/homepage/AllFriends';
 
 const Homepage = () => {
     return (
-        <div>
-            <Banner />
-            <AllFriends />
-            <Footer />
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-5xl px-0">
+                <Banner />
+                <AllFriends />
+            </div>
         </div>
     );
 };
